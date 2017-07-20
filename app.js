67f57140975b01197a55a0dff30dce91d9134060
Sheldon Santos <shelldroid01@gmail.com>
2017-07-20 00:05:06 -0300

    initial

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');  
+var app = express();  
+var server = require('http').createServer(app);  
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/node_modules'));  
+app.get('/', function(req, res,next) {  
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(client) {  
+    console.log('Client connected...');
+
+    client.on('join', function(data) {
+        console.log(data);
+        client.emit('messages', 'Hello from server');
+    });
+});
+
+
+server.listen(3001);

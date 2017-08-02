f76b79c80d62a355ce0932b8625e1ffcb77f1e75
sheldon_santos <shelldroid01@gmail.com>
2017-08-02 13:17:09 -0300

    add layout

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/node_modules'));  
+app.use(express.static(__dirname + '/public'));  
 app.get('/', function(req, res,next) {  
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(client) {  
+io.on('connection', function(client) {
     console.log('Client connected...');
 
     client.on('join', function(data) {
@@ -18,6 +19,7 @@ io.on('connection', function(client) {
 
   client.on('chat message', function(msg){
     console.log(msg);
+    client.emit('resposta', 'aqui do server');    
   });
 
 

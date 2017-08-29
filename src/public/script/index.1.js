a29131bf695a26e6d115b863ff2e5df320b282dc
sheldon_santos <shelldroid01@gmail.com>
2017-08-29 14:22:20 -0300

    outro layout

--- src/public/script/index.1.js
+++ src/public/script/index.1.js
@@ -1,16 +1,22 @@
 
 function addMessage(id) {
-    $('#messages').append(`
-    <div class="msg-item">
-    <b id="`+id+`user">User b</b><small id="time`+id+`" class="pull-right time"><i class="fa fa-clock-o"></i></small>
-    <pre id="msg`+id+`" class="msg-text">conteudo asdaga asdas asdfasd2</pre>
-  </div>`);
+    $('#messages').append(`<div class="media msg">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="">
+                    </a>
+                    <div class="media-body">
+                        <small id="time`+id+`" class="pull-right time"><i class="fa fa-clock-o"></i></small>
+                            <h5 id="`+id+`user" class="media-heading"></h5>
+
+                            <pre id="msg`+id+`" class="col-lg-10"></pre>
+                        </div>
+                    </div>`);
 }
 $(function () {
     var id = 0;
     var socket = io.connect();
     var shift = false;
-    var boxMessages = document.querySelector('#messages');
+    var boxMessages = document.querySelector('#boxMessages');
 
     $('#editor').keydown(function (event) { if (event.which == 16) shift = true });
     $('#editor').keyup(function (event) { if (event.which == 16) shift = false });
